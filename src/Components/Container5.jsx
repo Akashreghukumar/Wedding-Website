46a12b14d8bb10750eb7276ecc0929940d1413cf
Akashreghukumar <47936922+Akashreghukumar@users.noreply.github.com>
2023-01-28 00:23:11 +0530

    3 files modified

--- src/Components/Container5.jsx
+++ src/Components/Container5.jsx
@@ -6,12 +6,36 @@ import { con1image, img2 } from './../constants';
 export default function Container1() {
   return (
     <div>
-      <Container>
+      <Container className="d-flex flex-column gap-3">
         <h1>We Can't Wait to Celebrate with You</h1>
         <Row>
-          <input type="text" />
-          <input type="text" />
-          <input type="text" />
+          <Col>
+            <label>Name</label>
+          </Col>
+          <Col>
+            <input type="text" />
+          </Col>
+        </Row>
+        <Row>
+          <Col>
+            <label>Email</label>
+          </Col>
+          <Col>
+            <input type="text" />
+          </Col>
+        </Row>
+        <Row>
+          <Col>
+            <label>Will you being Attending?</label>
+          </Col>
+          <Col className="d-flex gap-3">
+            <label>Yes</label>
+            <input type="radio" name="confirm" value="Yes" />
+            <label>No</label>
+            <input type="radio" name="confirm" value="No" />
+            <label>May Be</label>
+            <input type="radio" name="confirm" value="Maybe" />
+          </Col>
         </Row>
       </Container>
     </div>
